34fcb6a41e0456cc1c0703a3f65acc3572a9e4f0
WingsofStars <taylorldelbridge@gmail.com>
2023-02-21 01:38:46 -0700

    Personal Information Filled Out and Website is fininshed, and deployed

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HostListener } from '@angular/core';
+import { trainingObject } from './training.model';
 
 @Component({
   selector: 'app-training',
@@ -8,6 +9,57 @@ import { HostListener } from '@angular/core';
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent {
+
+  arrayOFTrainingObjects : trainingObject[] = [
+    {
+      place: "Sololearn",
+    course: "Python Core",
+    Information: "Course that went over the general information regarding Python"
+    },
+    {
+      place: "Sololearn",
+    course: "Responsive Web Design",
+    Information: "A course that teaches HTML, CSS, and Javascript and how to make a responsive frontend"
+    },
+    {
+      place: "Sololearn",
+    course: "Java",
+    Information: "A course that teaches an overview of the programming language, Java."
+    },
+    {
+      place: "Sololearn",
+    course: "C#",
+    Information: "A course that goes into how to write, run, and create C# code, as well as it's attributes."
+    },
+    {
+      place: "Udemy",
+    course: "Learn Ethical Hacking from Scratch",
+    Information: "Become an ethical hacker that can hack computer systems like black hat hackers and secure them like security experts."
+    },
+    {
+      place: "Udemy",
+    course: "Learn Network Hacking From Scratch (Wifi & Wired)",
+    Information: "Learn how to hack both WiFi and wired networks like black hat hackers, and learn how to secure them from these attacks."
+    },
+    {
+      place: "Udemy",
+    course: "Python for Data Science and Machine Learning Bootcamp",
+    Information: "Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!"
+    },
+    {
+      place: "Udemy",
+    course: "Machine Learning A-Z™: Python & R in Data Science [2023]",
+    Information: "Learn to create Machine Learning Algorithms in Python and R from two Data Science experts. Code templates included."
+    },
+  ]
+
+
+
+
+
+
+
+
 screenHeight: number = window.innerHeight;
 screenWidth:number = window.innerWidth;
 blockIsValid:boolean = false;
---
+++ src/app/training/training.model.ts
@@ -0,0 +1,10 @@
+export class trainingObject {
+    place: string;
+    course: string;
+    Information: string;
+    constructor(place: string, course: string, Information: string){
+      this.place = place;
+      this.course = course;
+      this.Information = Information;
+    }
+}
